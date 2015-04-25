4ef943eb8df4ab991d4e7809af10536b07b11ef8
boxuanzhang <boxuan.zhang@student.qut.edu.au>
2015-04-26 02:04:16 +0800

    Implement basic ui

--- app/app/scripts/controllers/main.js
+++ app/app/scripts/controllers/main.js
@@ -9,9 +9,12 @@
  */
 angular.module('qutTigersApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.images = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    $scope.loadMore = function() {
+      var last = $scope.images[$scope.images.length - 1];
+      for(var i = 1; i <= 20; i++) {
+        $scope.images.push(last + i);
+      }
+    };
   });
