453b8fd56e2a1ad27996e240da3922e7deb4470e
boxuanzhang <boxuan.zhang@student.qut.edu.au>
2015-04-25 12:27:42 +0800

    Project skeleton

---
+++ app/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name qutTigersApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the qutTigersApp
+ */
+angular.module('qutTigersApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
